cf204b2301d802fa762dab316f4e514bfd117557
Sander van den Hoed <s.vdhoed@gmail.com>
2021-04-03 14:19:42 +0200

    Added AggressionPCT

--- Strategies/Simple.ps1
+++ Strategies/Simple.ps1
@@ -6,15 +6,18 @@ foreach ($market in $markets) {
     $bagPct = [float] $market.vol_sells_worth / ([float] $market.managed_value_usd / 100)
 
     $bemPct = "0"
+    $aggressivenessPct = "15"
     
     if ($24hPriceChange -le -12) {
-        $bemPct = "7"
+        $bemPct = "6"
+        $aggressivenessPct = "50"
     } 
     elseif ($24hPriceChange -le -8) {
         $bemPct = "4"
+        $aggressivenessPct = "25"
     } 
     elseif ($bagPct -le 40 -and $24hPriceChange -le -2) {
         $bemPct = "2"
     }
-    Set-GSMGSetting -Market $marketName -BemPct $bemPct
+    Set-GSMGSetting -Market $marketName -BemPct $bemPct -AggressivenessPct $aggressivenessPct
 }
