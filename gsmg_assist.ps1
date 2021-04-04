7f9e22b5def9aa2208dbf12140785f6961ba5d33
Sander van den Hoed <s.vdhoed@gmail.com>
2021-04-04 15:37:47 +0200

    Moved validation if script exists into while loop

--- gsmg_assist.ps1
+++ gsmg_assist.ps1
@@ -6,21 +6,22 @@ if (-not $curPath) {
 if (-not (Test-Path "$curPath\parameters.ps1")) {
     throw "Parameter file needs to be created before running the tool, see readme.md"
 }
+
 . "$curPath\parameters.ps1"
 . "$curPath\Functions\binance_api.ps1"
 . "$curPath\Functions\gsmg_api.ps1"
 . "$curPath\Functions\Converters.ps1"
 . "$curPath\Functions\Tools.ps1"
 
-$strategyPath = "$curPath\Strategies\$global:GSMGStrategy.ps1"
-if (-not (Test-Path $strategyPath)) {
-    throw "Could not find strategy for filename '$strategyPath'."
-}
-
 while ($true) {
     cls
 
-    . $strategyPath
+    $strategyPath = "$curPath\Strategies\$global:GSMGStrategy.ps1"
+    if (-not (Test-Path $strategyPath)) {
+        Write-Warning "Could not find strategy with filename '$strategyPath'."
+    } else {
+        . $strategyPath
+    }
 
     Write-Host "Sleeping for 60 seconds before running strategy again..."
     Sleep -Seconds 60
