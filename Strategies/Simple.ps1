70128e6f02ecea4d173fa97859358c98432678da
Sander van den Hoed <s.vdhoed@gmail.com>
2021-04-27 11:35:40 +0200

    Added a market reversal check

--- Strategies/Simple.ps1
+++ Strategies/Simple.ps1
@@ -2,17 +2,22 @@
 foreach ($market in $markets) {
     $marketName = $market.market_name.Replace("Binance:", "")
     [float] $pctChangeFromATH = Get-AthChangePct -Market $marketName -Interval "1d" -CandleLimit 1000
+    [float] $pctChange24h = (Query-24hTicker($marketName)).priceChangePercent
     $bagPct = [float] $market.vol_sells_worth / ([float] $market.managed_value_usd / 100)
 
     $bemPct = "-15"
     $aggressivenessPct = "10"
 
-    if ($pctChangeFromATH -le -35 -and $bagPct -lt 60) {
-        $bemPct = "2"
+    # Market is reversing after a downtrend??
+    if ($pctChange24h -gt -5)
+    {
+        if ($pctChangeFromATH -le -35 -and $bagPct -lt 60) {
+            $bemPct = "2"
+        }
+        elseif ($pctChangeFromATH -le -25) {
+            $bemPct = "0"
+        } 
     }
-    elseif ($pctChangeFromATH -le -25) {
-        $bemPct = "0"
-    } 
 
     #Write-Host "[$Marketname] $pctChangeFromAth, $bemPct, $aggressivenessPct"
     Set-GSMGSetting -Market $marketName -BemPct $bemPct -AggressivenessPct $aggressivenessPct
