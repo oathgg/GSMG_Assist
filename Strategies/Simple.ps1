e62dcf22e5417e1a3e284e05310349daa22efaf8
Sander van den Hoed <s.vdhoed@gmail.com>
2021-05-01 10:41:03 +0200

    Fixed typo

--- Strategies/Simple.ps1
+++ Strategies/Simple.ps1
@@ -23,7 +23,7 @@ foreach ($market in $markets) {
             $shouldAllocate = $true
         } 
         elseif ($pctChangeFromATH -le -10) {
-            $bemPct = "-3"
+            $bemPct = "-5"
             $shouldAllocate = $true
         } 
     }
@@ -60,6 +60,6 @@ foreach ($setting in $settings.GetEnumerator()) {
     }
 
     if ($curMarket.bem_pct -ne $newBem -or $curMarket.aggressiveness_pct -ne $newAgg) {
-        Set-GSMGSetting -Market $Setting.Key -BemPct $newBem -AggressivenessPct $newBem
+        Set-GSMGSetting -Market $Setting.Key -BemPct $newBem -AggressivenessPct $newAgg
     }
 }
