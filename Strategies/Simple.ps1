732b58c212a97a0fce46c2a2d28464a9c2a95359
Sander van den Hoed <s.vdhoed@gmail.com>
2021-05-04 11:48:10 +0200

    -10

--- Strategies/Simple.ps1
+++ Strategies/Simple.ps1
@@ -3,19 +3,25 @@
     $addDefault = $true
 
     foreach ($marketName in ($global:MarketsToScan | Sort-Object)) {
-        [float] $pctChangeFromATH = Get-AthChangePct -Market $marketName -Interval "1h" -CandleLimit 720 -IncludeCurrentCandle
-        #[float] $pctChangeFromATH = Get-PreviousHighFromCandles -Market $marketName -Interval "1h" -CandleLimit 720 -IncludeCurrentCandle
+        [float] $pctChangeFromATH = Get-AthChangePct -Market $marketName -Interval "1h" -CandleLimit 350 -IncludeCurrentCandle
+        #[float] $pctChangeFromATH = Get-PreviousHighFromCandles -Market $marketName -Interval "1d" -CandleLimit 40 -IncludeCurrentCandle
         
         [float] $pctChange24h = (Get-24hTicker($marketName)).priceChangePercent
+
         $market = $Global:GSMGmarkets | Where-Object { $_.market_name -eq $marketName }
         $allocation = $Global:GSMGAllocations | Where-Object { $_.market_name -match $marketName }
 
         if ($allocation) {
+            $allocatedBagPct = 0
             if ($allocation.set_alloc_perc -gt 0) {
-                $bagPct = [float] [Math]::Round(($allocation.open_sells_alloc_perc / $allocation.set_alloc_perc) * 100, 1)
-            } else {
-                $bagPct = [float] [Math]::Round(($allocation.open_sells_alloc_perc / $allocation.current_alloc) * 100, 1)
+                $allocatedBagPct = [float] [Math]::Round(($allocation.open_sells_alloc_perc / $allocation.set_alloc_perc) * 100, 1)
+            } 
+
+            $bagPct = [float] [Math]::Round(($allocation.open_sells_alloc_perc / $allocation.current_alloc) * 100, 1)
+            if ($allocatedBagPct -gt $bagPct) {
+                $bagPct = $allocatedBagPct
             }
+
             if ([Double]::IsNaN($bagPct)) {
                 $bagPct = 0
             }
@@ -28,7 +34,7 @@
         $bemPct = "-15"
         $aggressivenessPct = "10"
         $shouldAllocate = $false
-        $TrailingBuy = $false
+        $TrailingBuy = $true
 
         if ($addDefault) {
             $Settings += New-ConfigurationObject -BemPct $bemPct -AggressivenessPct $aggressivenessPct -ShouldAllocate $shouldAllocate -BaseCurrency "" -MarketName "DEFAULT" -MinProfitPct $minProfitPct
@@ -45,10 +51,17 @@
                 } else {
                     $bemPct = 0
                 }
+                $TrailingBuy = $false
                 $minProfitPct = 15
                 $shouldAllocate = $true
             }
             elseif ($pctChangeFromATH -le -30) {
+                if ($bagPct -gt 40) {
+                    $bemPct = -4
+                } else {
+                    $bemPct = 0
+                    $TrailingBuy = $false
+                }
                 $bemPct = 0
                 $minProfitPct = 10
                 $shouldAllocate = $true
@@ -58,20 +71,19 @@
                     $bemPct = -4
                 } else {
                     $bemPct = 0
+                    $TrailingBuy = $false
                 }
-                $TrailingBuy = $true
                 $minProfitPct = 5
                 $shouldAllocate = $true
             }
             # -15 might be too aggressive
-            <#elseif ($pctChangeFromATH -le -15) {
+            elseif ($pctChangeFromATH -le -10) {
                 if ($bagPct -lt 10) {
-                    $bemPct = 0
+                    $bemPct = -2
                     $minProfitPct = 1
                     $shouldAllocate = $true
-                    $TrailingBuy = $true
                 }
-            } #>
+            }
         }
 
         if ($shouldAllocate) {
