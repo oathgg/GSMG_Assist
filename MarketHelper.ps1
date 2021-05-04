62ac474383f57038fa8ec99ee847b78223a65cb2
Sander van den Hoed <s.vdhoed@gmail.com>
2021-05-04 19:15:52 +0200

    Reverted a lot of the changes back to default, simple.

--- MarketHelper.ps1
+++ MarketHelper.ps1
@@ -12,68 +12,8 @@
 }
 
 function Run-ConfigureGSMG($Settings) {
-    # Enables and disables markets which are no longer applicable.
-    # We keep track of the amount of markets which we want to disable but we cant,
-    # A reason why we can't would be because we still have open sell orders.
-    $marketsToDisable = @()
-    foreach ($activeAllocation in $Global:GSMGAllocations) {
-        $activeMarketName = $activeAllocation.Market_Name.Replace("Binance:", "")
-        $settingsMarket = $settings | Where-Object { $_.MarketName -eq $activeMarketName }
-        if (-not $settingsMarket.ShouldAllocate) {
-            $marketsToDisable += $activeMarketName
-        }
-    }
-
-    $marketsToUpdate = @()
-    foreach ($marketName in $marketsToDisable) {
-        $curMarket = $Global:GSMGmarkets | Where-Object { $_.market_name -eq $marketName }
-        $allocationActive = $Global:GSMGAllocations | Where-Object { $_.market_name -match $marketName }
-        $baseCurrency = $curMarket.base_currency
-
-        # The amount of money we still have open in the coin
-        if (-not $allocationActive -or ($allocationActive -and $allocationActive.managed_value_usd -lt 1)) {
-            Set-GMSGMarketStatus -Market $marketName -Enabled $False
-        } 
-        # We cannot disable this market because there is still too much money in it
-        else {
-            $defaultSettings = $Settings | Where-Object { $_.MarketName -eq "DEFAULT" }
-            $newBem = $defaultSettings.BemPCT
-            $newAgg = $defaultSettings.AggressivenessPct
-            $minProfitPct = $defaultSettings.MinProfitPct
-            $trailingBuy = $defaultSettings.TrailingBuy
-            Set-GSMGSetting -Market $marketName -BemPct $newBem -AggressivenessPct $newAgg -MinTradeProfitPct $minProfitPct -TrailingBuy $trailingBuy
-            
-            $marketsToUpdate += $settings | Where-Object { $_.Marketname -eq $marketname }
-            Write-Warning "[$marketname] Cannot disable market, managed value : $($allocationActive.managed_value_usd)"
-        }
-
-        # Make sure we dont have any allocation left when we disable the market
-        # We set it to 0 in case we do have some of our bag left, this way we "leave" the market but sell orders remain open.
-        if ($null -ne $curMarket.allocation -and $curMarket.allocation -ne 0) {
-            Set-GMSGMarketAllocation -Market $marketName -AllocationPct 0
-        }
-    }
-
-    # Calculates the amount of markets we want to enable so we don't cross the max market count
-    $availableMarketSlots = (Get-GSMGSubscription).max_markets_across_exchanges
-    $availableMarketSlots -= $marketsToUpdate.Count
-    if ($availableMarketSlots -gt 0) {
-        foreach ($marketToAdd in $settings | Where-Object { $_.ShouldAllocate }) {
-            if ($marketsToUpdate -notcontains $marketToAdd.MarketName) {
-                $marketsToUpdate += $marketToAdd
-
-                $availableMarketSlots--
-                if ($availableMarketSlots -eq 0) {
-                    break;
-                }
-            }
-        }
-    } else {
-        Write-Warning "No more slots available"
-    }
-
     # Enable the markets we want to enable and set the predefined settings for that particular market.
-    foreach ($setting in $marketsToUpdate) {
+    foreach ($setting in $Settings) {
         $marketName = $setting.MarketName
         $curMarket = $Global:GSMGmarkets | Where-Object { $_.market_name -eq $marketName }
         $allocationActive = $Global:GSMGAllocations | Where-Object { $_.market_name -match $marketName }
@@ -96,17 +36,7 @@ function Run-ConfigureGSMG($Settings) {
             $allocPct = $global:MaxAllocationPct[$baseCurrency]
         }
 
-        if ($shouldAlloc -and (-not $allocationActive -or $allocationActive.current_alloc -ne $allocPct)) {
-            Set-GMSGMarketStatus -Market $marketName -Enable $True
-        }
-
-        # Reduce spam by checking if we're actually changing anything to what the server has
-        if ($curMarket.allocation -ne $allocPct) {
-            Set-GMSGMarketAllocation -Market $marketName -AllocationPct $allocPct
-        }
-
-        #if ($curMarket.bem_pct -ne $newBem -or $curMarket.aggressiveness_pct -ne $newAgg -or $curMarket.min_trade_profit_pct -ne $minProfitPct) {
-            Set-GSMGSetting -Market $marketName -BemPct $newBem -AggressivenessPct $newAgg -MinTradeProfitPct $minProfitPct -TrailingBuy $trailingBuy
-        #}
+        Set-GMSGMarketAllocation -Market $marketName -AllocationPct $allocPct
+        Set-GSMGSetting -Market $marketName -BemPct $newBem -AggressivenessPct $newAgg -MinTradeProfitPct $minProfitPct -TrailingBuy $trailingBuy
     }
 }
--- Strategies/Simple.ps1
+++ Strategies/Simple.ps1
@@ -1,11 +1,8 @@
 ﻿function Run-Strategy() {
     $Settings = @();
-    $addDefault = $true
-
-    foreach ($marketName in ($global:MarketsToScan | Sort-Object)) {
-        [float] $pctChangeFromATH = Get-AthChangePct -Market $marketName -Interval "1h" -CandleLimit 350 -IncludeCurrentCandle
-        #[float] $pctChangeFromATH = Get-PreviousHighFromCandles -Market $marketName -Interval "1d" -CandleLimit 40 -IncludeCurrentCandle
-        
+    $marketsToScan = $Global:GSMGAllocations.Market_name.Replace("Binance:", "")
+    foreach ($marketName in $marketsToScan) {
+        [float] $pctChangeFromATH = Get-AthChangePct -Market $marketName -Interval "1d" -CandleLimit 30 -IncludeCurrentCandle
         [float] $pctChange24h = (Get-24hTicker($marketName)).priceChangePercent
 
         $market = $Global:GSMGmarkets | Where-Object { $_.market_name -eq $marketName }
@@ -13,87 +10,56 @@
 
         if ($allocation) {
             $bagPct = 0
-
-            # The bag pct based on our allocation pct which we have set
-            $setAllocBagPct = 0
             if ($allocation.set_alloc_perc -gt 0) {
-                $setAllocBagPct = [float] [Math]::Round(($allocation.open_sells_alloc_perc / $allocation.set_alloc_perc) * 100, 1)
-            } 
-
-            # The bag % based on our currently allocated amount
-            $currentlyAllocatedBagPct = 0
-            if ($allocation.current_alloc -gt 1) {
-                $currentlyAllocatedBagPct = [float] [Math]::Round(($allocation.open_sells_alloc_perc / $allocation.current_alloc) * 100, 1)
-            }
-
-            # Check which one is the highest
-            if ($setAllocBagPct -gt $currentlyAllocatedBagPct) {
-                $bagPct = $setAllocBagPct
+                $bagPct = [float] [Math]::Round(($allocation.open_sells_alloc_perc / $allocation.set_alloc_perc) * 100, 1)
             } else {
-                $bagPct = $currentlyAllocatedBagPct
-            }
-
-            if ([Double]::IsNaN($bagPct)) {
-                $bagPct = 0
+                $bagPct = [float] [Math]::Round(($allocation.open_sells_alloc_perc / $allocation.current_alloc) * 100, 1)
             }
         } else {
             $bagPct = 0
         }
 
         # Default settings
-        $minProfitPct = 1;
+        $minProfitPct = 5;
         $bemPct = "-15"
         $aggressivenessPct = "10"
         $shouldAllocate = $false
         $TrailingBuy = $true
 
-        if ($addDefault) {
-            $Settings += New-ConfigurationObject -BemPct $bemPct -AggressivenessPct $aggressivenessPct -ShouldAllocate $shouldAllocate -BaseCurrency "" -MarketName "DEFAULT" -MinProfitPct $minProfitPct
-            $addDefault = $false
-        }
-
         # Market is reversing after a downtrend
         # We do not want to spend money when the market has been going up too fast
         if ($pctChange24h -gt -5 -and $pctChange24h -lt 10)
         {
-            if ($pctChangeFromATH -le -40) {
-                if ($bagPct -gt 60) {
-                    $bemPct = -4
-                } else {
+            if ($pctChangeFromATH -le -35) {
+                if ($bagPct -le 60) {
                     $bemPct = 2
+                } else {
+                    $bemPct = 0
                 }
                 $TrailingBuy = $false
                 $minProfitPct = 15
                 $shouldAllocate = $true
             }
-            elseif ($pctChangeFromATH -le -30) {
-                if ($bagPct -gt 40) {
-                    $bemPct = -4
-                } else {
+            elseif ($pctChangeFromATH -le -25) {
+                if ($bagPct -le 40) {
                     $bemPct = 2
+                } else {
+                    $bemPct = 0
                     $TrailingBuy = $false
                 }
                 $bemPct = 0
                 $minProfitPct = 10
                 $shouldAllocate = $true
             }
-            elseif ($pctChangeFromATH -le -20) {
-                if ($bagPct -gt 20) {
-                    $bemPct = -4
-                } else {
+            elseif ($pctChangeFromATH -le -15) {
+                if ($bagPct -le 20) {
                     $bemPct = 0
+                } else {
+                    $bemPct = -2
                     $TrailingBuy = $false
                 }
                 $minProfitPct = 5
                 $shouldAllocate = $true
-            }
-            # Might be too aggressive
-            elseif ($pctChangeFromATH -le -15) {
-                if ($bagPct -lt 10) {
-                    $bemPct = -2
-                    $minProfitPct = 1
-                    $shouldAllocate = $true
-                }
             }
         }
 
