96301252ec9c6798a50203766252042ee6fe6228
Sander van den Hoed <s.vdhoed@gmail.com>
2021-05-26 12:49:11 +0200

    new calculation for top 10

--- Get-ProfitableMarkets.ps1
+++ Get-ProfitableMarkets.ps1
@@ -1,7 +1,10 @@
-﻿. "$curPath\Functions\binance_api.ps1"
+﻿$curPath = $PSScriptRoot
+if (-not $curPath) {
+    $curPath = $psise.CurrentFile.FullPath
+}
+
+. "$curPath\Functions\binance_api.ps1"
 . "$curPath\Functions\gsmg_api.ps1"
-. "$curPath\Functions\Converters.ps1"
-. "$curPath\Functions\Tools.ps1"
 . "$curPath\parameters.ps1"
 
 Clear-Host
@@ -11,19 +14,9 @@ $count = 0
 
 foreach ($pm in $profitableMarkets) {
     $marketName = $pm.market_name
-    [float] $pctChange24h = (Get-24hTicker($marketName)).priceChangePercent
+    $pm | Select-Object Market_name, market_knowledge_b
 
-    if ($pctChange24h -gt -10 -and $pctChange24h -lt 15) {
-        [float] $pctChangeFromATH = Get-AthChangePct -Market $marketName -Interval "1d" -CandleLimit 30 -IncludeCurrentCandle
-        if ($pctChangeFromATH -le -15) {
-            $count++
-            $pm | Select-Object Market_name, market_knowledge_b
-        }
-    }
-
-    if ($count -eq 10) {
+    if ($count++ -eq 10) {
         break;
     }
 }
-
-#$profitableMarkets | Select-Object -First 10 | Select-Object Market_name, market_knowledge_b
